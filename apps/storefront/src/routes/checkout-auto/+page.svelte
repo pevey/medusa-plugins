f7a87ce6e1257ac75d44493e9c9dc7e1f07c8eb0
pevey <7490308+pevey@users.noreply.github.com>
2026-07-24 15:20:41 -0500

    checkout experiments

---
+++ apps/storefront/src/routes/checkout-auto/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { STRIPE_KEY, STRIPE_REDIRECT_URL, GOOGLE_PLACES_API_KEY } from '$app/env/public'
+	import { CheckoutAuto } from '$lib/components/ui/checkout/index.js'
+</script>
+
+<CheckoutAuto
+	publishableKey={STRIPE_KEY}
+	returnUrl={STRIPE_REDIRECT_URL}
+	googlePlacesApiKey={GOOGLE_PLACES_API_KEY}
+	config={{ pp_stripe_stripe: { elements: true } }}
+/>
---
+++ apps/storefront/src/routes/checkout-stripe/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import { STRIPE_KEY, STRIPE_REDIRECT_URL } from '$app/env/public'
+	import { CheckoutStripe } from '$lib/components/ui/checkout/index.js'
+</script>
+
+<CheckoutStripe publishableKey={STRIPE_KEY} returnUrl={STRIPE_REDIRECT_URL} />
