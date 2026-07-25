fc138af0bc783b08b043f66ca61c608bd51bbb42
pevey <7490308+pevey@users.noreply.github.com>
2026-07-24 21:29:17 -0500

    checkout updates

---
+++ apps/storefront/src/routes/checkout/return/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import { STRIPE_KEY } from '$app/env/public'
+	import { Return } from '$lib/components/ui/checkout/index.js'
+</script>
+
+<Return publishableKey={STRIPE_KEY} />
